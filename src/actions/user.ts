0b80183bb055831711b0713fd6655ecfcc55fe1b
gsistelos <gabrielsistelos@gmail.com>
2024-05-21 16:53:57 -0300

    register page

--- src/actions/user.ts
+++ src/actions/user.ts
@@ -1,5 +1,19 @@
 import { prisma } from "@/db";
 
+export async function createUser(formData: FormData) {
+  const username = formData.get("username") as string;
+  const email = formData.get("email") as string;
+  const password = formData.get("password") as string;
+
+  return await prisma.user.create({
+    data: {
+      username,
+      email,
+      password,
+    },
+  });
+}
+
 export async function getUsers() {
   return await prisma.user.findMany();
 }
