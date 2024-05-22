e2ed8943d7eecafcb435ece8e291fb9d863e2cc0
gsistelos <gabrielsistelos@gmail.com>
2024-05-22 02:06:36 -0300

    actually fix creating multiple prisma clients in dev mode

--- src/db.ts
+++ src/db.ts
@@ -1,3 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient();
+function createPrismaClient() {
+  if (process.env.NODE_ENV === "production") {
+    return new PrismaClient();
+  }
+
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+
+  return global.prisma;
+}
+
+const prisma = createPrismaClient();
+
+export { prisma };
